3e6c6d5fca7ec9deb226f3101bdd409f6d107d5e
MrLetsplay2003 <mr.letsplay2003@gmail.com>
2022-06-05 22:12:57 +0200

    Update CssElement

--- src/main/java/me/mrletsplay/simplehttpserver/dom/css/CssElement.java
+++ src/main/java/me/mrletsplay/simplehttpserver/dom/css/CssElement.java
@@ -5,43 +5,53 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 public class CssElement {
-	
+
 	private CssSelector selector;
 	private Map<String, Supplier<String>> properties;
-	
+
 	public CssElement(CssSelector selector) {
 		this.selector = selector;
 		this.properties = new HashMap<>();
 	}
-	
+
 	public void setSelector(CssSelector selector) {
 		this.selector = selector;
 	}
-	
+
 	public CssSelector getSelector() {
 		return selector;
 	}
-	
+
 	public void setProperty(String name, Supplier<String> value) {
+		if(value == null) {
+			properties.remove(name);
+			return;
+		}
+
 		properties.put(name, value);
 	}
-	
+
 	public void setProperty(String name, String value) {
+		if(value == null) {
+			properties.remove(name);
+			return;
+		}
+
 		setProperty(name, () -> value);
 	}
-	
+
 	public Supplier<String> getProperty(String name) {
 		return properties.get(name);
 	}
-	
+
 	public Map<String, Supplier<String>> getProperties() {
 		return properties;
 	}
-	
+
 	public boolean isEmpty() {
 		return properties.isEmpty();
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder b = new StringBuilder();
