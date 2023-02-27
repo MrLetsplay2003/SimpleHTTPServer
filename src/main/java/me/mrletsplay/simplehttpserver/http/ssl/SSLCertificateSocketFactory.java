4f5c3ee94cbcd19611db6ef05ed4ea9f38121fa6
null234 <43322985+x0rp01s0n@users.noreply.github.com>
2023-02-27 17:31:14 +0100

    Update SSLCertificateSocketFactory.java

--- src/main/java/me/mrletsplay/simplehttpserver/http/ssl/SSLCertificateSocketFactory.java
+++ src/main/java/me/mrletsplay/simplehttpserver/http/ssl/SSLCertificateSocketFactory.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
